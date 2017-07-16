6a8a578f2c034e5c89d3ee98ec4100b952379e43
Benjamin <benj@zagorski.com>
2017-07-15 18:06:23 -0700

    Moved drinkList to seperate document

---
+++ script/drinkList.js
@@ -0,0 +1,54 @@
+const DRINKS = [
+  {
+    name: "Cape Cod",
+    low_carb: false,
+  },
+  {
+    name: "Greyhound",
+    low_carb: false,
+  },
+  {
+    name: "Screwdriver",
+    low_carb: false,
+  },
+  {
+    name: "Bloody Mary",
+    low_carb: false,
+  },
+  {
+    name: "Margarita",
+    low_carb: false,
+  },
+  {
+    name: "Manhattan",
+    low_carb: true,
+  },
+  {
+    name: "Moscow Mule",
+    low_carb: false,
+  },
+  {
+    name: "Scotch Neat",
+    low_carb: true,
+  },
+  {
+    name: "Gin and Tonic",
+    low_carb: false,
+  },
+  {
+    name: "Gin and Soda",
+    low_carb: true,
+  },
+  {
+    name: "Beer",
+    low_carb: false,
+  },
+  {
+    name: "Wine",
+    low_carb: false,
+  },
+  {
+    name: "Champagne",
+    low_carb: false,
+  },
+];
--- script/drinks.js
+++ script/drinks.js
@@ -1,9 +1,10 @@
+var DRINKS;
 
 function generateDrink() {
   var drinks;
-  drinks = $("#low-carb")[0].checked ? selectLowCarb() : DRINKS
+  drinks = $("#low-carb")[0].checked ? selectLowCarb() : DRINKS;
   var drink = drinks[Math.floor(Math.random()*drinks.length)];
-  $("#response").html(drink.name)
+  $("#response").html(drink.name);
 }
 
 function selectLowCarb() {
@@ -12,46 +13,7 @@ function selectLowCarb() {
     if (drink.low_carb) {
       drinks.push(drink);
     }
-  })
+  });
 
   return drinks;
 }
-
-const DRINKS = [
-  {
-    name: "Margarita",
-    low_carb: false,
-  },
-  {
-    name: "Manhattan",
-    low_carb: true,
-  },
-  {
-    name: "Moscow Mule",
-    low_carb: false,
-  },
-  {
-    name: "Scotch Neat",
-    low_carb: true,
-  },
-  {
-    name: "Gin and Tonic",
-    low_carb: false,
-  },
-  {
-    name: "Gin and Soda",
-    low_carb: true,
-  },
-  {
-    name: "Beer",
-    low_carb: false,
-  },
-  {
-    name: "Wine",
-    low_carb: false,
-  },
-  {
-    name: "Champagne",
-    low_carb: false,
-  },
-];

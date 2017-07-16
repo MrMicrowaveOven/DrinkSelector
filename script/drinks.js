6fe89997e7db30af0a7de30caac73758a3541c3b
Benjamin <benj@zagorski.com>
2017-07-15 20:20:16 -0700

    Added ingredients listed as 'special'

--- script/drinks.js
+++ script/drinks.js
@@ -1,16 +1,35 @@
 var DRINKS_LIST = DRINKS;
 
+var ingredientsList = {};
+DRINKS_LIST.forEach(function(drink) {
+  var ingredientName;
+  drink.ingredients.forEach(function(ingredient) {
+    ingredientName = ingredient.ingredient;
+    if (ingredientsList[ingredientName]) {
+      ingredientsList[ingredientName] += 1;
+    } else {
+      ingredientsList[ingredientName] = 1;
+    }
+  });
+});
+// console.log(Object.keys(ingredientsList).length);
+// Object.keys(ingredientsList).forEach(function(ingredient) {
+//   console.log(ingredient);
+// });
+
 function generateDrink() {
   var drinks;
-  drinks = $("#low-carb")[0].checked ? selectLowCarb() : DRINKS_LIST;
+  drinks = DRINKS_LIST;
   var drink = drinks[Math.floor(Math.random()*drinks.length)];
   $("#response").html("");
   $("#response").append("<div id='drinkName'>" + drink.name + "<div>");
   if (drink.ingredients.length > 0) {
-    $("#response").append("<div>" + "Ingredients:" + "<div>");
+    // $("#response").append("<div>" + "Ingredients:" + "<div>");
     drink.ingredients.forEach(function(ingredient) {
       if (ingredient.ingredient) {
         $("#response").append("<div>" + ingredient.ingredient + "<div>");
+      } else if (ingredient.special) {
+        $("#response").append("<div>" + ingredient.special + "<div>");
       }
     });
   }

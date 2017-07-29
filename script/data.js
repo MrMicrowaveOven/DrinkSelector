5637f5334fffbd6fe78710d4d3c77bbe6ea5b872
Benjamin <benj@zagorski.com>
2017-07-28 20:12:56 -0700

    Data log is now within a helper function

--- script/data.js
+++ script/data.js
@@ -7,53 +7,55 @@ var ingredients = {};
 var garnishes = {};
 var preparations = {};
 
-DRINKLIST.forEach(function(drink) {
+function getDrinkData() {
+  DRINKLIST.forEach(function(drink) {
 
-  if (categories[drink.category]) {
-    categories[drink.category] += 1;
-  } else {
-    categories[drink.category] = 1;
-  }
-
-  if (glasses[drink.glass]) {
-    glasses[drink.glass] += 1;
-  } else {
-    glasses[drink.glass] = 1;
-  }
-
-  if (garnishes[drink.garnish]) {
-    garnishes[drink.garnish] += 1;
-  } else {
-    garnishes[drink.garnish] = 1;
-  }
-
-  if (preparations[drink.preparation]) {
-    preparations[drink.preparation] += 1;
-  } else {
-    preparations[drink.preparation] = 1;
-  }
-
-  drink.ingredients.forEach(function(ingredientSpecs) {
-    if (units[ingredientSpecs.unit]) {
-      units[ingredientSpecs.unit] += 1;
+    if (categories[drink.category]) {
+      categories[drink.category] += 1;
     } else {
-      units[ingredientSpecs.unit] = 1;
+      categories[drink.category] = 1;
     }
 
-    if (amounts[ingredientSpecs.amount]) {
-      amounts[ingredientSpecs.amount] += 1;
+    if (glasses[drink.glass]) {
+      glasses[drink.glass] += 1;
     } else {
-      amounts[ingredientSpecs.amount] = 1;
+      glasses[drink.glass] = 1;
     }
 
-    if (ingredients[ingredientSpecs.ingredient]) {
-      ingredients[ingredientSpecs.ingredient] += 1;
+    if (garnishes[drink.garnish]) {
+      garnishes[drink.garnish] += 1;
     } else {
-      ingredients[ingredientSpecs.ingredient] = 1;
+      garnishes[drink.garnish] = 1;
     }
+
+    if (preparations[drink.preparation]) {
+      preparations[drink.preparation] += 1;
+    } else {
+      preparations[drink.preparation] = 1;
+    }
+
+    drink.ingredients.forEach(function(ingredientSpecs) {
+      if (units[ingredientSpecs.unit]) {
+        units[ingredientSpecs.unit] += 1;
+      } else {
+        units[ingredientSpecs.unit] = 1;
+      }
+
+      if (amounts[ingredientSpecs.amount]) {
+        amounts[ingredientSpecs.amount] += 1;
+      } else {
+        amounts[ingredientSpecs.amount] = 1;
+      }
+
+      if (ingredients[ingredientSpecs.ingredient]) {
+        ingredients[ingredientSpecs.ingredient] += 1;
+      } else {
+        ingredients[ingredientSpecs.ingredient] = 1;
+      }
+    });
   });
-
-});
+}
+getDrinkData();
 // console.log(glasses);
 // console.log(categories);
 // console.log(garnishes);
